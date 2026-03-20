9ca90658ce1c387a1621851dc69b2a78adde6cf0
Sophia Shapira <sophiashapira@Sophias-MacBook-Pro.local>
2026-03-20 01:22:50 -0400

    Created the basic -cartovanta- tool and installer.

---
+++ cartovanta.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/env perl
+# cartovanta - Main command line tool for the CartoVanta card system
+# Copyright (C) 2026  Sophia Elizabeth Shapira
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+use strict;
+
+my @arguma; # Environments to pass to the subcommand
+my $subcm; # Subcommand to search for
+my $counto; # Used for argument counting:
+
+my $pathvar; # The path along wich to search for subcommands:
+my @pathpart; # $pathvar after being chopped by colon
+my $patheach; # Each element of path
+my $possib; # Each possible location of subcommand
+
+# Process the command line:
+@arguma = @ARGV;
+$counto = @arguma;
+if ( $counto < 0.5 )
+{
+  die("\nFATAL ERROR:\n  Usage: cartovanta [subcommand] ([arguments])\n\n");
+}
+$subcm = shift(@arguma);
+
+# Put together the value for $pathvar
+$pathvar = $ENV{'CARTOVANTA_PATH'};
+if ( $pathvar ne '' ) { $pathvar .= ':'; }
+$pathvar .= $ENV{'HOME'} . '/local/cartovanta-bin';
+$pathvar .= ':/usr/local/cartovanta-bin';
+
+@pathpart = split(quotemeta(':'),$pathvar);
+foreach $patheach (@pathpart)
+{
+  $possib = $patheach . '/' . $subcm;
+  if ( -f $possib )
+  {
+    if ( -x $possib )
+    {
+      exec($possib,@arguma);
+    }
+  }
+}
+
+die("\nFATAL ERROR:\n No such -cartovanta- subcommand: " . $subcm . " :\n\n");
+
+
---
+++ install.pl
@@ -0,0 +1,80 @@
+#!/usr/bin/env perl
+# install.pl - Main install script for -cartovanta-
+# Copyright (C) 2026  Sophia Elizabeth Shapira
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+use strict;
+
+my @pathset; # All locations on the PATH
+my $bindest; # Install destination for -cartovanta-
+
+# Find @pathset
+{
+  my $lc_a;
+  $lc_a = $ENV{'PATH'};
+  @pathset = split(quotemeta(':'),$lc_a);
+}
+
+# Find $bindest
+$bindest = &findbin;
+sub findbin {
+  my $lc_hme;
+  my $lc_pos;
+  $lc_hme = $ENV{'HOME'};
+  if ( $lc_hme eq '' )
+  {
+    $lc_hme = `(cd && pwd)`; chomp($lc_hme);
+  }
+  $lc_pos = $lc_hme . '/local/bin';
+  if ( &found_in_path($lc_pos) ) { return $lc_pos; }
+  $lc_pos = $lc_hme . '/bin';
+  if ( &found_in_path($lc_pos) ) { return $lc_pos; }
+  die(
+    "\nFATAL ERROR:\n  Could not find where to install -cartovanta-\n" .
+    "  Please make sure one of the following two is\n" .
+    "        on the \$PATH environment variable:\n" .
+    "    " . $lc_hme . "/local/bin\n" .
+    "    " . $lc_hme . "/bin\n" .
+  "\n");
+}
+
+# And when I try ot find $bindest, I will need to check if
+# a given possibility is on the PATH
+sub found_in_path {
+  my $lc_a;
+  foreach $lc_a (@pathset)
+  {
+    if ( $lc_a eq $_[0] ) { return(2>1); }
+  }
+  return(1>2);
+}
+
+# Go ahead and install the thing
+{
+  my $lc_ds;
+  $lc_ds = ($bindest . '/cartovanta');
+  system('mkdir','-p',$bindest);
+  if ( -d $lc_ds )
+  {
+    die("\nFATAL ERROR:\n  Why is a directory present at this location?\n  " . $lc_ds . "\n\n");
+  }
+  system('cp','cartovanta.pl',$lc_ds);
+  system('chmod','755',$lc_ds);
+}
+
+
+
+
+
+
